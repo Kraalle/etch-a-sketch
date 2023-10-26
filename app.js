2f9a9c75a2d7e11db2e99fd300bad23492b23e46
Max McLaren <maxtova@yahoo.com>
2023-10-26 10:03:25 -0700

    finalized javascript for project. Now draws with hover and also changes div size while using same number of pixels

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
 const container = document.querySelector('#container');
+let gridSize = 16;
+const colors = [];
 
-for (i = 0; i < 16 * 16; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
+// creates divs inside the container
+function createGrid(size) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    const cellSize = 800 / size;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.width = `${cellSize}px`;
+        box.style.height = `${cellSize}px`;
+        container.appendChild(box);
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'green';
+        })
+    }
 }
+
+
+
+// generates a random color for drawing
+function randomColorGenerator() {
+    for (let i = 0; i < 50; i++) {
+        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        colors.push(randomColor);
+    }
+}
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    let divSize;
+    let isValid = false;
+
+    while (!isValid) {
+        divSize = prompt('Choose your div size!');
+        if (divSize === null) {
+            break;
+        }
+
+        const number = parseInt(divSize);
+
+        if (!isNaN(number)) {
+            isValid = true;
+            gridSize = number;
+            createGrid(gridSize);
+        } else {
+            alert("Please enter a valid number.")
+        }
+    }
+    
+})
+
+createGrid(gridSize);
+
